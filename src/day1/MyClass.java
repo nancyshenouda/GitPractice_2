d8754e6c95c900edc74259aa9a39a604dbc4445a
nancyshenouda <Shenouda.nancy@gmail.com>
2021-04-24 17:31:30 -0400

    new line added

--- src/day1/MyClass.java
+++ src/day1/MyClass.java
@@ -3,5 +3,6 @@ package day1;
 public class MyClass {
     public static void main(String[] args) {
         System.out.println("I started to like GitHub");
+        System.out.println("New line added");
     }
 }
